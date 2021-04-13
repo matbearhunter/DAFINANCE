d00219ec9f698e64adc93c3903ad2cc0b6d336cc
Mateo <sainamateo@gmail.com>
2021-04-13 14:09:31 +0100

    Initilized my repo and added m1c1 and m1c2

---
+++ M1CH1.py
@@ -0,0 +1,57 @@
+
+print(8 / 2)
+print(2**2)
+print(4*4)
+
+# Print the total revenue
+total = 140+15+3
+print(total)
+
+# Print the average revenue
+average = total/3
+print(average)
+
+# Create company_1
+company_1 = ("Apple")
+print(company_1)
+# Create year_1
+year_1 = 2017
+print(year_1)
+# Create revenue_1
+revenue_1 = 229.23
+print(revenue_1)
+
+# Type of company_1
+print(type(company_1))
+
+# Type of year_1
+print(type(year_1))
+
+# Type of revenue_1
+print(type(revenue_1))
+
+test_company = 'apple'
+company_1==test_company
+
+# Compare revenue_1 and revenue_2
+print(revenue_1>revenue_2)
+
+# Update data types
+year_1_str = str(year_1)
+revenue_1_str = str(revenue_1)
+
+# Create a complete sentence combining only the string data types
+sentence = 'The revenue of ' + company_1 + ' in ' + str(2017) + ' was $' + str(229.23) + ' billion.'
+
+# Print sentence
+print(sentence)
+
+# Update data types
+year_1_str = str(year_1)
+revenue_1_str = str(revenue_1)
+
+# Create a complete sentence combining only the string data types
+sentence = 'The revenue of ' + company_1 + ' in ' + str(2017) + ' was $' + str(229.23) + ' billion.'
+
+# Print sentence
+print(sentence)
